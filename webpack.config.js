42eaa0f59845157f2a7e13e23a004c098c27c296
hihello-bye <137717369+hihello-bye@users.noreply.github.com>
2024-09-18 17:09:20 +0100

    Ass css loader

--- webpack.config.js
+++ webpack.config.js
@@ -18,4 +18,13 @@ module.exports = {
       template: "./src/index.html",
     }),
   ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
 };
+

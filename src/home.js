38efd98b1bc353f82403a8ccaf8a53031b429543
hihello-bye <137717369+hihello-bye@users.noreply.github.com>
2024-09-19 15:33:43 +0100

    Add about paragraph

--- src/home.js
+++ src/home.js
@@ -7,6 +7,11 @@ function loadHomePage() {
     homeTitle.textContent = 'Sanrio Cafe';
     homeTitle.classList.add('home-title');
 
+    const homeAbout = document.createElement('p');
+    homeAbout.textContent = 'Enjoy great food with Hello Kitty and all her friends! Our Sanrio inspired menu is as cute as it is tasty. Try different dishes and drinks inspired by Sanrio characters.';
+    homeAbout.classList.add('home-about');
+
+    content.appendChild(homeAbout);
     content.appendChild(homeTitle);
 
 }

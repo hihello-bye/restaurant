a8bbcd5e7db9e5ba7b2dbd1279da3c15eca792a3
hihello-bye <137717369+hihello-bye@users.noreply.github.com>
2024-09-19 14:22:19 +0100

    Add entry points and load home page initially when page loads

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 import "./style.css";
 
+import {loadHomePage} from './src/home.js';
+import {loadMenuPage} from './src/menu.js';
+
+window.addEventListener('DOMContentLoaded', () => {
+    loadHomePage();
+})
+
+
 console.log("Yummy food");

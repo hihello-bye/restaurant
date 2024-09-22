1e933a95601ae70bd6878f969676105c90a7a1b7
hihello-bye <137717369+hihello-bye@users.noreply.github.com>
2024-09-22 15:26:36 +0100

    Switch between tabs when button clicked

--- src/index.js
+++ src/index.js
@@ -5,7 +5,14 @@ import {loadMenuPage} from './menu.js';
 
 window.addEventListener('DOMContentLoaded', () => {
     loadHomePage();
-})
+});
 
+homeButton.addEventListener('click', () => {
+    loadHomePage();
+});
+
+menuButton.addEventListener('click', () => {
+    loadMenuPage();
+});
 
 console.log("Yummy food");
